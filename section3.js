cf142a490462fd5036de1860f1cbe076eaea3d14
aydaBastami <aydabstmi@gmail.com>
2021-05-20 14:30:20 -0700

    ida2

--- section3.js
+++ section3.js
@@ -2,5 +2,14 @@ const form = document.querySelector("#FormTweet")
 const tweet = document.querySelector("#tweet")
 
 form.addEventListener("submit", e =>{
-    
+
 })
+
+const addTweet =  (username, tweet) => {
+    const NewTweet = document.createElement("li")
+    const UserB = document.createElement("b")
+    UserB.append(username)
+    NewTweet.append(UserB)
+    NewTweet.append(`- ${tweet}`)
+    tweet.append(NewTweet)
+}
